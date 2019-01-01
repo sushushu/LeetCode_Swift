984458b462646d17930dcba26e469413c91b23b0
sushushu <1404565175@qq.com>
2019-01-01 23:43:04 +0800

    929. 独特的电子邮件地址

--- LeetCode_Swift/Easy.swift
+++ LeetCode_Swift/Easy.swift
@@ -52,3 +52,42 @@ class Solution_771 {
         }
     }
 }
+
+// https://leetcode-cn.com/problems/unique-email-addresses/
+//每封电子邮件都由一个本地名称和一个域名组成，以 @ 符号分隔。
+//
+//例如，在 alice@leetcode.com中， alice 是本地名称，而 leetcode.com 是域名。
+//
+//除了小写字母，这些电子邮件还可能包含 ',' 或 '+'。
+//
+//如果在电子邮件地址的本地名称部分中的某些字符之间添加句点（'.'），则发往那里的邮件将会转发到本地名称中没有点的同一地址。例如，"alice.z@leetcode.com” 和 “alicez@leetcode.com” 会转发到同一电子邮件地址。 （请注意，此规则不适用于域名。）
+//
+//如果在本地名称中添加加号（'+'），则会忽略第一个加号后面的所有内容。这允许过滤某些电子邮件，例如 m.y+name@email.com 将转发到 my@email.com。 （同样，此规则不适用于域名。）
+//
+//可以同时使用这两个规则。
+//
+//给定电子邮件列表 emails，我们会向列表中的每个地址发送一封电子邮件。实际收到邮件的不同地址有多少？
+class Solution_929 {
+    func numUniqueEmails(_ emails: [String]) -> Int {
+        var tmpList = Array<String>()
+        var resList = Array<String>()
+        // 规则处理
+        for email in emails {
+            var prefix = String(email.split(separator: "@", maxSplits: 1, omittingEmptySubsequences: false)[0])
+            let suffix = String(email.split(separator: "@", maxSplits: 1, omittingEmptySubsequences: false)[1])
+            prefix = String(prefix.split(separator: "+", maxSplits: 1, omittingEmptySubsequences: false)[0]) // 碰到+号只取前面的部分
+            prefix = prefix.replacingOccurrences(of: ".", with: "") // 碰到.号 去除
+            tmpList.append("\(prefix)@\(suffix)") // 重组邮箱 加进数组做去重处理
+        }
+        // 去重处理
+        for value in tmpList {
+            if !resList.contains(value) {
+                resList.append(value)
+            }
+        }
+//        print(resList)
+        return resList.count
+    }
+}
+
+
