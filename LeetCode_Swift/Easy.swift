5a0f8dabd70fda666d3bb1aed19c564cb989a6d6
sushushu <1404565175@qq.com>
2020-03-30 03:44:04 +0800

    链表

--- LeetCode_Swift/Easy.swift
+++ LeetCode_Swift/Easy.swift
@@ -408,6 +408,44 @@ class Solution_206 {
     }
 }
 
+// https://leetcode-cn.com/problems/linked-list-cycle/
+//给定一个链表，判断链表中是否有环。
+//
+//为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+//
+//
+//
+//示例 1：
+//
+//输入：head = [3,2,0,-4], pos = 1
+//输出：true
+//解释：链表中有一个环，其尾部连接到第二个节点。
+//
+//
+//示例 2：
+//
+//输入：head = [1,2], pos = 0
+//输出：true
+//解释：链表中有一个环，其尾部连接到第一个节点。
+//
+//
+//示例 3：
+//
+//输入：head = [1], pos = -1
+//输出：false
+//解释：链表中没有环。
+//进阶：
+//
+//你能用 O(1)（即，常量）内存解决此问题吗？
+
+class Solution_141 {
+    func hasCycle(_ head: ListNode?) -> Bool {
+        
+        
+        return false
+    }
+}
+
 
 //// https://leetcode-cn.com/problems/zai-pai-xu-shu-zu-zhong-cha-zhao-shu-zi-lcof/
 //统计一个数字在排序数组中出现的次数。
